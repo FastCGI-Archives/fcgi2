ed7372550a861471ea9e7ea08109e0034ecf6960
robs <robs>
2001-06-18 14:25:47 +0000

    Remove a bunch of includes that don't belong here.

--- include/fcgimisc.h
+++ include/fcgimisc.h
@@ -9,40 +9,12 @@
  * See the file "LICENSE.TERMS" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * $Id: fcgimisc.h,v 1.2 1999/05/28 02:19:58 roberts Exp $
+ * $Id: fcgimisc.h,v 1.3 2001/06/18 14:25:47 robs Exp $
  */
 
 #ifndef _FCGIMISC_H
 #define _FCGIMISC_H
 
-#include <stdio.h>
-#include <limits.h>
-
-#include <fcgi_config.h>
-
-#ifdef HAVE_SYS_SELECT_H
-#include <sys/select.h>
-#endif
-#include <sys/types.h>
-#ifdef HAVE_SYS_SOCKET_H
-#include <sys/socket.h>
-#endif
-#ifdef HAVE_WINSOCK_H
-#include <winsock.h>
-#endif
-#ifdef HAVE_NETINET_IN_H
-#include <netinet/in.h>
-#endif
-#ifdef HAVE_ARPA_INET_H
-#include <arpa/inet.h>
-#endif
-#ifdef HAVE_SYS_UN_H
-#include <sys/un.h>
-#endif
-
-/*
- * Where does this junk normally come from?
- */
 #ifndef FALSE
 #define FALSE (0)
 #endif
