d840662027e42079f535ad297d1fd1e1d55c83ee
Boris Pek <tehnick-8@yandex.ru>
2018-10-16 01:29:14 +0300

    Add clean.sh

---
+++ clean.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+rm -rf \
+    */Makefile.in \
+    COPYING \
+    INSTALL \
+    Makefile.in \
+    aclocal.m4 \
+    autom4te.cache/ \
+    compile \
+    config.* \
+    configure \
+    depcomp \
+    fcgi_config.h.in* \
+    install-sh \
+    ltmain.sh \
+    m4/ \
+    missing
+
